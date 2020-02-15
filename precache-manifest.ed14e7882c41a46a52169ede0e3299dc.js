77fea22c7ad001ba8bb42d4680289146c84735f5
Claudio Castro <claudioluiz.castro@gmail.com>
2020-02-15 19:49:57 -0300

    Deploy GhPages II

---
+++ precache-manifest.ed14e7882c41a46a52169ede0e3299dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-movies/robots.txt"
+  },
+  {
+    "revision": "d61f85608ea1aad1f8ab",
+    "url": "/vue-movies/js/chunk-vendors.da7c77fb.js"
+  },
+  {
+    "revision": "ff67395b7112ef527c29",
+    "url": "/vue-movies/js/app.320f07c8.js"
+  },
+  {
+    "revision": "48c5b51dab69de2e368886291384cdeb",
+    "url": "/vue-movies/index.html"
+  },
+  {
+    "revision": "ff67395b7112ef527c29",
+    "url": "/vue-movies/css/app.f87f27a2.css"
+  }
+];
