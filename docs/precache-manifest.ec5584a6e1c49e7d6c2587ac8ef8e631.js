a76a5247d1d0fdc3129038aac69989087f6d9c5c
Claudio Castro <claudioluiz.castro@gmail.com>
2020-02-15 19:33:17 -0300

    Deploy GitHub Pages I

---
+++ docs/precache-manifest.ec5584a6e1c49e7d6c2587ac8ef8e631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d61f85608ea1aad1f8ab",
+    "url": "/js/chunk-vendors.da7c77fb.js"
+  },
+  {
+    "revision": "a33c1249f55106a7eb47",
+    "url": "/js/app.5cbb45ae.js"
+  },
+  {
+    "revision": "83c41d07f50781c4d5b2a498299d47eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a33c1249f55106a7eb47",
+    "url": "/css/app.f87f27a2.css"
+  }
+];
